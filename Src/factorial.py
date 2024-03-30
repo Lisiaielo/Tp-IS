ea7c8097a5e1200f6250ebbe45753bea1fd437e1
Liset <lisiaielo@gmail.com>
2024-03-30 15:02:46 -0300

    se hizo la segunda modificacion

--- Src/factorial.py
+++ Src/factorial.py
@@ -6,22 +6,27 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
-def factorial(num): 
-    if num < 0: 
+
+def factorial(num):
+    if num < 0:
         print("El factorial de un número negativo no existe")
-    elif num == 0: 
+        return None
+    elif num == 0:
         return 1
-    else: 
+    else:
         fact = 1
-        while num > 1: 
-            fact *= num 
+        while num > 1:
+            fact *= num
             num -= 1
-        return fact 
+        return fact
+
+def calcular_factoriales(rango):
+    inicio, fin = map(int, rango.split('-'))
+    for num in range(inicio, fin + 1):
+        print("El factorial de", num, "! es", factorial(num))
 
 if len(sys.argv) == 1:
-    print("Debe informar un número como argumento.")
-    num = int(input("Ingrese un número: "))
+    print("Debe informar un rango de números como argumento (ejemplo: 4-8).")
+    rango = input("Ingrese un rango de números (ejemplo: 4-8): ")
 else:
-    num = int(sys.argv[1])
-
-print("El factorial de", num, "! es", factorial(num))
+    rango = sys.argv[1]
